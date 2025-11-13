ee47f0125df19d3c8da4f2a95409c04f2236b8ac
setukuru <raymondsetukuru@gmail.com>
2025-11-13 15:32:09 +0200

    deploy frontend

--- client/config.js
+++ client/config.js
@@ -1,12 +1,11 @@
 const config = {
-  // API Configuration
+
   API_BASE_URL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8800/api",
-  
-  // Socket Configuration
+
   SOCKET_BASE_URL: import.meta.env.VITE_SOCKET_BASE_URL || "http://localhost:5001",
-  
-  // Client URL
+
   CLIENT_URL: import.meta.env.VITE_CLIENT_URL || "http://localhost:5174"
+  
 };
 
 export default config;
