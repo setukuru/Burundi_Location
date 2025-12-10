3bc9be353136bed4faea1ac51b83d24fe384ecd3
setukuru <133489021+setukuru@users.noreply.github.com>
2025-12-10 17:32:07 +0200

    Update app.js

--- api/app.js
+++ api/app.js
@@ -32,7 +32,7 @@ app.use(
       }
     },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
   })
 );
